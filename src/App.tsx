11cc4d41633084250124a28a819e3c16d2ad62f9
Terence Thambi Rajah <terence_thambi@yahoo.com>
2024-07-16 10:17:22 +1000

    ExpandedText example

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 //import Alert from "./components/Alert";
 //import Button from "./components/Button";
 import Like from "./components/Like";
+import ExpandableText from "./components/ExpandableText";
 //import ListGroup from "./components/ListGroup";
 
 // function App() {
@@ -23,13 +24,53 @@ function App() {
   // let items = ["Singapore", "Melbourne", "Sydney"];
   // const heading = "Cities";
 
-  const onClick = () => {
-    console.log("Clicked!!!");
+  // const [game, setGame] = useState({
+  //   id: 1,
+  //   player: {
+  //     name: "John",
+  //   },
+  // });
+
+  //const [pizza, setPizza] = useState({
+  //   name: "Spicy Peperroni",
+  //   toppings: ["Mushroom"],
+  // });
+
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
+
+  const handleClick = () => {
+    //setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    //console.log(pizza);
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+    console.log(cart);
   };
 
+  // return (
+  //   <div>
+  //     <Like onClick={onClick}></Like>
+  //   </div>
+  // );
+  //   return (
+  //     <div>
+  //       <button onClick={handleClick}>
+  //         Click Me {cart.items[0].quantity}!!!
+  //       </button>
+  //     </div>
+  //   );
   return (
     <div>
-      <Like onClick={onClick}></Like>
+      <ExpandableText>Hello Word</ExpandableText>
     </div>
   );
 }
